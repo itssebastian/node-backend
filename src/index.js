cc954ca69b909265ca247710cc9724743775603a
Sebastian <sebastianvannier@outlook.com>
2024-10-08 13:19:07 +0530

    Utils code of Api Error and Api Response

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 // require ('dotenv').config({path:'./env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`!! Server is running at port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed ", err);
+  });
 
 // import express from "express";
 // const app = express()(async () => {
