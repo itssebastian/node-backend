0a1d6318a6cafb0ad4820eaec39e487c2b512439
Sebastian <sebastianvannier@outlook.com>
2024-10-11 10:09:59 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8000;
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`!! Server is running at port: ${PORT}`);
+      console.log(`⚙️  Server is running at port: ${PORT}`);
     });
   })
   .catch((err) => {
